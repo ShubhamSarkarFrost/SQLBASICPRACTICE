e6e2b682f62ae12ecff4bd303a608d718ab92181
frost <ronniecse8@gmail.com>
2024-08-24 15:21:44 +0530

    UPDATED SQL AddTables - 24-08-2024-15:21_IST

---
+++ AddTables.sql
@@ -0,0 +1,28 @@
+CREATE TABLE covid_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    country VARCHAR(50),
+    total_cases VARCHAR(100),
+    total_deaths VARCHAR(100),
+    total_recovered VARCHAR(100),
+    active_cases VARCHAR(100),
+    serious_cases VARCHAR(100),
+    top_cases VARCHAR(100),
+	deaths_cases VARCHAR(100),
+    total_tests VARCHAR(100),
+    tested_positive VARCHAR(100),
+    population VARCHAR(100)
+);
+
+SHOW VARIABLES LIKE 'secure_file_priv';
+
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/CovidData.csv'
+INTO TABLE covid_data
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+DESCRIBE covid_data;  -- to Describe the columns 
+
+select * from covid_data
--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS test_database; -- Create a Database if does not exist
-USE test_database;    -- use the newly created Database
+CREATE DATABASE IF NOT EXISTS covid_database; -- Create a Database if does not exist
+USE covid_database;    -- use the newly created Database
 show databases;   -- show all the Databases 
 select database();   -- Show the Current Database Used
